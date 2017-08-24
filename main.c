e46a10ace4324bf5fbe2fbb5ea815e47d9854e63
Matias Rozanec <rozanecm@gmail.com>
2017-08-24 18:46:37 -0300

    mejora en msj de help.

--- main.c
+++ main.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 void printHelpData(){
-	printf("%s\n\t%s\n\t%s\n\t%s\n%s\n\t%s\t%s\n\t%s\t%s\n\t%s\t%s\n\t%s\
-\t%s\n\t%s\t%s\n\t%s\t%s\n",\
-      	"Usage:", "tp0 -h", "tp0 -V", "tp0 [options] file", "Options:", \
-		"-V, --version", "Print version and quit.", "-h, --help", \
-		"Print this information.", "-o, --output", "Path to output file.", \
-		"-i, --input", "Path to input file.", "-q, --qsort", "Use quicksort.", \
-		"-b, --bsort", "Use bubblesort.");
+	printf("Usage:\n \ttp0 -h \n\ttp0 -V \n\ttp0 [options] file \nOptions: \n\t-V, --version \tPrint version and quit. \n\t-h, --help \tPrint this information. \n\t-o, --output \tPath to output file. \n\t-i, --input \tPath to input file. \n\t-q, --qsort \tUse quicksort. \n\t-b, --bsort \tUse bubblesort.\n");
 }
 
 int main(int argc, char* argv[]){
-	if (strcmp(argv[1], "-h") == 0)
-		printHelpData();
-	//TODO check this if statement; use strcmp
-	else if (argv[1] == "-V" || argv[1] == "--version")
-//		printVersionNumber();
-
+	if (argc > 1){
+		if (strcmp(argv[1], "-h") == 0){
+			printHelpData();
+		}
+		//TODO check this if statement; use strcmp
+		else if (argv[1] == "-V" || argv[1] == "--version"){
+	//		printVersionNumber();
+		}
+	}
 	return 0;
 }
