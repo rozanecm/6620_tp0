184333347e6f95d3790cd7383b659324fcd9132e
Matias Rozanec <rozanecm@gmail.com>
2017-08-29 19:41:49 -0300

    solve argument parsing. Parse methods not implemented yet.

--- src/main.c
+++ src/main.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
 #include <string.h>
-void printHelpData(){
-	printf("Usage:\n \ttp0 -h \n\ttp0 -V \n\ttp0 [options] file \nOptions: \n\t-V, --version \tPrint version and quit. \n\t-h, --help \tPrint this information. \n\t-o, --output \tPath to output file. \n\t-i, --input \tPath to input file. \n\t-q, --qsort \tUse quicksort. \n\t-b, --bsort \tUse bubblesort.\n");
+#include <getopt.h>
+
+void print_help_data(){
+	printf("Usage:\n \terat-h \n\terat -V \n\terat [options] file \nOptions: \n\t-V, --version \tPrint version and quit. \n\t-h, --help \tPrint this information. \n\t-o, --output \tPath to output file. \n\t-i, --input \tPath to input file. \n\t-q, --qsort \tUse quicksort. \n\t-b, --bsort \tUse bubblesort.\n");
 }
 
+void print_version_info(){};
+
+void calculate_sieve_of_Eratosthenes(char* path){};
+
 int main(int argc, char* argv[]){
-	if (argc > 1){
-		if (strcmp(argv[1], "-h") == 0){
-			printHelpData();
-		}
-		//TODO check this if statement; use strcmp
-		else if (argv[1] == "-V" || argv[1] == "--version"){
-	//		printVersionNumber();
-		}
+	static struct option long_options[] = {
+		{"help", 	no_argument, 		0, 'h' },
+		{"version",	no_argument, 		0, 'v' },
+		{"output",	required_argument,	0, 'o' },
+		{0,			0,					0,  0  }
+	};
+	int option_index = 0;
+	int c = getopt_long(argc, argv, "hvo:", long_options, &option_index); 
+	switch(c){
+		case 'h':
+			print_help_data();
+		case 'v':
+			print_version_info();
+		case 'o':
+			calculate_sieve_of_Eratosthenes(optarg);
+		default:
+			printf("?? getopt returned character code 0%o ??\n", c);
 	}
 	return 0;
 }
