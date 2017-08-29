443105874c68077d6976c81387445729cb40b21c
Matias Rozanec <rozanecm@gmail.com>
2017-08-29 20:02:03 -0300

    complete version funtion

--- src/main.c
+++ src/main.c
@@ -6,7 +6,9 @@ void print_help_data(){
 	printf("Usage:\n \terat-h \n\terat -V \n\terat [options] N \nOptions:\n\t-h, --help \tPrint usage information.\n\t-V, --version \tPrints version information.\n\t-o, --output\tPath to output file.\nExamples:\n\terat -o - 10\n");
 }
 
-void print_version_info(){};
+void print_version_info(){
+	printf("Version 1.0\n");
+};
 
 void calculate_sieve_of_Eratosthenes(char* path){};
 
