fb7ad821739c6bb6dab5932c2fa1b20163a32ce1
Matias Rozanec <rozanecm@gmail.com>
2017-09-27 18:58:49 -0300

    all corrections made according to teachers requests. All code is cpplint checked.

--- src/main.c
+++ src/main.c
@@ -3,7 +3,9 @@
 #include <string.h>
 #include <getopt.h>
 #include <math.h>
-#define MAX_INPUT 1000000
+
+/* define max input. Number is max supported for 32 bit int */
+#define MAX_INPUT 2147483647
 
 
 void print_help_data(){
@@ -18,7 +20,6 @@ void print_version_info(){
 }
 
 void erat(unsigned int * numeros,unsigned int N){
-
 	int i = 2;
 	int j = 2;
 	
@@ -30,10 +31,7 @@ void erat(unsigned int * numeros,unsigned int N){
 		i++;
 		j = 2;
 	}
-	
 }
-
-
 
 void print_sieve_of_Eratosthenes(char* path, unsigned int N){
     /* pi(N): In mathematics, the prime-counting function is the function
@@ -47,26 +45,36 @@ void print_sieve_of_Eratosthenes(char* path, unsigned int N){
 	erat(all_nums, N);
 
 	/* check where to print results */
+
+    /* According to ASCII table, 45 is the '-' char.
+     * Source: http://www.asciitable.com/ */
 	if (*path == 45){
-		// output to stdout
+		/* output to stdout */
         for (int i = 2; i <= N; ++i){
-        	if(all_nums[i]!=0)
-           		printf("%i\n", all_nums[i]);
+        	if(all_nums[i]!=0){
+                printf("%i\n", all_nums[i]);
+            }
         }
 	}else{
 		FILE* file = fopen(path, "w");
+        /* error checking */
+        if (file == NULL){
+            exit(EXIT_FAILURE);
+        }
+
         for (int i = 2; i <= N; ++i){
-        	if(all_nums[i]!=0)
-            	fprintf(file, "%i\n", all_nums[i]);
+        	if(all_nums[i]!=0){
+                fprintf(file, "%i\n", all_nums[i]);
+            }
         }
 		fclose(file);
 	}
-
 }
 
 void check_input_num_and_print_sieve(char* path, int N){
 	if (N > MAX_INPUT){
-		fprintf(stderr, "Given number (%i) exceeded max input %i.\n", N, MAX_INPUT);
+		fprintf(stderr, "Given number (%i) exceeded max input %i.\n", N,
+                MAX_INPUT);
 	}else if (N < 2){
 		fprintf(stderr, "Given number (%i) is not a valid number\n", N);
 	}else{
@@ -74,7 +82,21 @@ void check_input_num_and_print_sieve(char* path, int N){
 	}
 }
 
+void print_sieve(int N) {
+    check_input_num_and_print_sieve("-", N);
+}
+
 int main(int argc, char* argv[]){
+    if (argc == 1){
+        /* piped input cant be longer than 15 char */
+        char stdin_buff[10];
+        if (fgets(stdin_buff, 15, stdin) == NULL){
+            /* if getting data from stdin fails, exit */
+            exit(EXIT_FAILURE);
+        }
+        print_sieve(atoi(stdin_buff));
+        return 0;
+    }
 	static struct option long_options[] = {
 		{"help", 	no_argument, 		0, 'h' },
 		{"version",	no_argument, 		0, 'v' },
@@ -94,7 +116,7 @@ int main(int argc, char* argv[]){
 			check_input_num_and_print_sieve(optarg, atoi(argv[argc-1]));
 			break;
 		default:
-			printf("Invalid option, run ./erat -h for help\n");
+            print_sieve(atoi(argv[argc-1]));
 	}
 	return 0;
 }
